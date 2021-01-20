2723baae91e932625b1ec30d3f3c65f2d8181503
Han-Weng <hanweng9@gmail.com>
2021-01-20 17:00:51 -0500

    6.4 update article

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles
 end
 
-Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
-
-
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
